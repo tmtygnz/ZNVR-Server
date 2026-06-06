8013c1f1886e86aac3bd21170a6b1b4323647756
!!Timothy <jostimian@gmail.com>
2026-06-06 12:07:17 +0800

    feat: Status getter

--- internal/camera/handler.go
+++ internal/camera/handler.go
@@ -8,28 +8,41 @@ import (
 	"golang.org/x/sync/errgroup"
 )
 
+type OnlineStatus int
+
+const (
+	Online = iota
+	StartingUp
+	Offline
+)
+
 type CameraHandler struct {
 	streamer streamer.Streamer
 	context  context.Context
+	status   OnlineStatus
 }
 
 func CreateCameraHandler(streamer streamer.Streamer, ctx context.Context) *CameraHandler {
 	return &CameraHandler{
 		streamer: streamer,
 		context:  ctx,
+		status:   Offline,
 	}
 }
 
 func (ch *CameraHandler) StartAllFunctions() {
 	g, ctx := errgroup.WithContext(ch.context)
+
 	g.Go(func() error {
 		return ch.streamer.StartRecording(ctx)
 	})
 	g.Go(func() error {
 		return ch.streamer.StartAIStreaming(ctx)
 	})
+	ch.status = Online
 
 	if err := g.Wait(); err != nil {
 		slog.Error("Camera function returned", "err", err.Error())
+		ch.status = Offline
 	}
 }
